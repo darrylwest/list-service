70b110a4eeb3e79f49154c859afa7dd883d4eed8
darryl west <darwest@ebay.com>
2018-01-17 14:03:12 -0800

    added list data model

---
+++ src/lister/list-model.go
@@ -0,0 +1,25 @@
+//
+// list model - a datamodel for list objects
+//
+// @author darryl.west@ebay.com
+// @created 2018-01-17 12:57:59
+//
+
+package lister
+
+import (
+    "time"
+)
+
+type List struct {
+    ID string
+    dateCreated time.Time
+    lastUpdated time.Time
+    version int64
+    title string
+    category string
+    attributes map[string]interface{}
+    status string
+}
+
+
---
+++ test/unit/list-model_test.go
@@ -0,0 +1,29 @@
+//
+// list model tests
+//
+// @author darryl.west <darwest@ebay.com>
+// @created 2018-01-17 08:35:20
+//
+
+package unit
+
+import (
+	"fmt"
+	"lister"
+	"testing"
+
+	. "github.com/franela/goblin"
+)
+
+func TestListModel(t *testing.T) {
+	g := Goblin(t)
+
+	g.Describe("ListModel", func() {
+		lister.CreateLogger()
+
+		g.It("should create a list struct", func() {
+			model := lister.List{}
+			g.Assert(fmt.Sprintf("%T", model)).Equal("lister.List")
+		})
+	})
+}
