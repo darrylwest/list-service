8fad7986c32ce50d7508546f9ac915424de7e686
darryl west <darryl.west@RainCitySoftware.com>
2018-01-29 15:48:38 -0800

    added service-net to enable caddy proxy

--- docker/run.sh
+++ docker/run.sh
@@ -3,10 +3,12 @@
 # 2017-12-29 11:29:48
 #
 
+docker network create service-net > /dev/null 2&>1
+
 name=list-service
 port=9040
 
-docker run -itd --name $name -p $port:80 -v $HOME/database:/data darrylwest/list-service
+docker run -itd --name $name -p $port:80 -v $HOME/database:/data --network=service-net darrylwest/list-service
 
 sleep 2
 curl http://localhost:$port/status | jq '.'
